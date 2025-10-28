4fadd4d47d568cf04c52ff2f65cd56e279b363f3
topherez <topherez@gmail.com>
2025-10-28 09:34:46 -0600

    .

--- lib/whop-sdk.ts
+++ lib/whop-sdk.ts
@@ -9,15 +9,6 @@ export const whopSdk = WhopServerSdk({
 	// You can get this from the Whop dashboard after creating an app section.
 	appApiKey: process.env.WHOP_API_KEY ?? "fallback",
 
-	// This will make api requests on behalf of this user.
-	// This is optional, however most api requests need to be made on behalf of a user.
-	// You can create an agent user for your app, and use their userId here.
-	// You can also apply a different userId later with the `withUser` function.
-	onBehalfOfUserId: process.env.NEXT_PUBLIC_WHOP_AGENT_USER_ID,
-
-	// This is the companyId that will be used for the api requests.
-	// When making api requests that query or mutate data about a company, you need to specify the companyId.
-	// This is optional, however if not specified certain requests will fail.
-	// This can also be applied later with the `withCompany` function.
-	companyId: process.env.NEXT_PUBLIC_WHOP_COMPANY_ID,
+	// Do NOT set default scoping here. We'll scope per-request using verifyUserToken + withUser/withCompany.
+	// onBehalfOfUserId and companyId are intentionally omitted to prevent accidental cross-company scoping.
 });
